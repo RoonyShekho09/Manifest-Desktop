fc00d1944929eb08926ef9ce07ec9fe3451e0738
Roony <roonyshekho@gmail.com>
2026-03-11 12:21:28 +0300

    Edit AppTextField, add icons and modify window size

--- composeApp/src/jvmMain/kotlin/com/example/myapplication/main.kt
+++ composeApp/src/jvmMain/kotlin/com/example/myapplication/main.kt
@@ -3,12 +3,10 @@ package com.example.myapplication
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import com.example.myapplication.utils.painter
 import me.sample.library.resources.Res
-import me.sample.library.resources.ic_check
 import me.sample.library.resources.ic_jawharat
 
 fun main() = application {
@@ -17,7 +15,7 @@ fun main() = application {
         title = "Manifest",
         resizable = false,
         icon = Res.drawable.ic_jawharat.painter,
-        state = rememberWindowState(size = DpSize(900.dp, 700.dp))
+        state = rememberWindowState(size = DpSize(1000.dp, 800.dp))
     ) {
         App()
     }
--- composeApp/src/jvmMain/kotlin/com/example/myapplication/presentation/components/AppTextField.kt
+++ composeApp/src/jvmMain/kotlin/com/example/myapplication/presentation/components/AppTextField.kt
@@ -1,9 +1,10 @@
 package com.example.myapplication.presentation.components
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -24,38 +25,37 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
-/**
- * @param state The current textfield state of the field.
- * @param modifier The modifier to be applied to the composable.
- * @param placeholder The placeholder text to display when the field is empty.
- * @param keyboardOptions Options for the software keyboard (e.g., KeyboardType.Text).
- * @param leadingIcon A composable function for an optional icon/content at the start of the field.
- * @param inputTransformation The inputTransformation transformation applied to the text (e.g., for passwords).
- */
+
 @Composable
 fun AppTextField(
     state: TextFieldState,
     modifier: Modifier = Modifier,
     placeholder: String = "Enter text here...",
+    readOnly: Boolean = false,
     suffixText: String? = null,
     keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
     onKeyboardAction: KeyboardActionHandler? = null,
     outputTransformation: OutputTransformation? = null,
     leadingIcon: @Composable (() -> Unit)? = null,
     trailingIcon: @Composable (() -> Unit)? = null,
-    inputTransformation: InputTransformation = InputTransformation
+    inputTransformation: InputTransformation? = null
 ) {
     BasicTextField(
         state = state,
-        textStyle = LocalTextStyle.current.copy(color = MaterialTheme.colorScheme.onSurface),
+        textStyle = LocalTextStyle.current.copy(
+            color = MaterialTheme.colorScheme.onSurface,
+            fontSize = 14.sp
+        ),
         keyboardOptions = keyboardOptions,
         cursorBrush = SolidColor(MaterialTheme.colorScheme.primary),
         onKeyboardAction = onKeyboardAction,
         outputTransformation = outputTransformation,
         inputTransformation = inputTransformation,
         lineLimits = TextFieldLineLimits.SingleLine,
-        modifier = modifier.height(56.dp),
+        readOnly = readOnly,
+        modifier = modifier.defaultMinSize(minHeight = 40.dp),
         decorator = object : TextFieldDecorator {
             @Composable
             override fun Decoration(innerTextField: @Composable () -> Unit) {
@@ -82,12 +82,16 @@ private fun TextFieldDecorator(
     innerTextField: @Composable () -> Unit
 ) {
     Surface(
-        shape = RoundedCornerShape(16.dp),
-        color = MaterialTheme.colorScheme.surfaceVariant.copy(alpha = 0.5f)
+        shape = RoundedCornerShape(4.dp),
+        color = MaterialTheme.colorScheme.surfaceVariant.copy(alpha = 0.3f),
+        border = BorderStroke(
+            width = 1.dp,
+            color = MaterialTheme.colorScheme.outline.copy(alpha = 0.5f)
+        )
     ) {
         Row(
             verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.padding(8.dp).height(56.dp) // Move height here
+            modifier = Modifier.padding(horizontal = 12.dp, vertical = 8.dp)
         ) {
             leadingIcon?.invoke()
             if (leadingIcon != null) Spacer(Modifier.width(8.dp))
@@ -96,14 +100,19 @@ private fun TextFieldDecorator(
                 if (state.text.isBlank()) {
                     Text(
                         text = placeholder,
-                        color = MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = 0.7f)
+                        color = MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = 0.7f),
+                        fontSize = 14.sp
                     )
                 }
                 innerTextField()
             }
 
             suffixText?.let {
-                Text(text = it, modifier = Modifier.padding(end = 16.dp))
+                Text(
+                    text = it,
+                    modifier = Modifier.padding(start = 8.dp),
+                    fontSize = 14.sp
+                )
             }
             trailingIcon?.invoke()
         }
