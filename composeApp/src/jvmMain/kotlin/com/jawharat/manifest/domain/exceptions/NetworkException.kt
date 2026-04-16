794420a399f1eb2e839045105630b7d51f31b588
Roony <roonyshekho@gmail.com>
2026-04-16 13:38:57 +0300

    Add TooManyRequests to NetworkException

--- composeApp/src/jvmMain/kotlin/com/jawharat/manifest/domain/exceptions/NetworkException.kt
+++ composeApp/src/jvmMain/kotlin/com/jawharat/manifest/domain/exceptions/NetworkException.kt
@@ -2,4 +2,5 @@ package com.jawharat.manifest.domain.exceptions
 
 sealed class NetworkException : Exception() {
     data class TokenExpiredException(override val message: String = "Token has expired") : Exception()
+    data class TooManyRequests(override val message: String = "Too many requests") : Exception()
 }
